d785881cb65c283746dd15dcd922b816855bf729
Sumeet <sumeetghumare4@gmail.com>
2026-02-27 10:24:23 +0530

    init

---
+++ backend/index.ts
@@ -0,0 +1,16 @@
+import express  from "express";  
+
+const app = express();
+
+console.log(process.env.DATABASE_URL);
+console.log(process.env.PORT);
+
+app.get("/", (req, res) => {
+    res.json({
+        db: process.env.DATABASE_URL,
+        port: process.env.PORT
+    })
+})
+
+app.listen(process.env.PORT);
+
